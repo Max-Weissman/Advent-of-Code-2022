243d71b5cf85c31080cb6924883f17d1536d3a26
Max <maxweissman@rogers.com>
2022-12-04 00:31:23 -0500

    Day 4 Star 1 Campsite Cleanup

---
+++ Day-4/Star_1/Cleanup.js
@@ -0,0 +1,40 @@
+var fs = require("fs");
+
+let counter = () => {
+    fs.readFile("./input.txt", "utf8", function(err, text){
+        let data = text.split('\n')
+        let total = 0
+        data.forEach(pair => {
+            let comma
+            for (let i = 0; i < pair.length; i++){
+                if (pair[i] === ","){
+                    comma = i
+                }
+            }
+            let one = pair.slice(0, comma) //seperate the two elves sections
+            let two = pair.slice(comma + 1)
+            let oneth
+            let twoth
+            for (let i = 0; i < one.length; i++){ //seperate the numbers in the ranges in upper and lower limit
+                if (one[i] === "-"){
+                    oneth = i
+                }
+            }
+            for (let i = 0; i < two.length; i++){
+                if (two[i] === "-"){
+                    twoth = i
+                }
+            }
+            oneLower = Number(one.slice(0, oneth))
+            oneUpper = Number(one.slice(oneth + 1))
+            twoLower = Number(two.slice(0, twoth))
+            twoUpper = Number(two.slice(twoth + 1))
+            if ((oneLower <= twoLower && oneUpper >= twoUpper) || (oneLower >= twoLower && oneUpper <= twoUpper)){ //if one section encompasses the other
+                total++
+            }
+        })
+        console.log(total)
+    });
+}
+
+counter()
